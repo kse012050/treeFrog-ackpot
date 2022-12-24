591f4b8204c308dafeeb33438c3c0a12531d1f18
김성은 <kse012050@naver.com>
2022-12-24 20:03:27 +0900

    JS 인풋 작업 중

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    // 인풋 유효성 검사
+    inputValidation()
+})
+
+// 인풋 유효성 검사
+function inputValidation(){
     $('[data-input="mobile"]').on('input' ,function(){
         !/^01(\d{9,9})/.test($(this).val()) ? $(this).next().addClass('active') : $(this).next().removeClass('active')
         submitActive();
@@ -7,38 +13,29 @@ $(document).ready(function(){
         !/\d{6,6}/.test($(this).val()) ? $(this).next().addClass('active') : $(this).next().removeClass('active')
         submitActive();
     })
-    function submitActive(){
-        $('input[type="submit"]').click(function(e){
-            !$(this).hasClass('active') && e.preventDefault();
-        })
-        console.log($('p').get());
-        let a = $('p').get().every((a)=>{
-            return !$(a).hasClass('active');
-        })
-        console.log(a);
-        let inputBool = [];
-        $('input[type="text"] , input[type="password"] , input[type="number"]').each(function(){
-            if(inputBool[0] === false) return;
-            if($(this).val() === "" && inputBool[0]){
-                inputBool[0] = false;
-                return;
-            }
-            inputBool[0] = true;
-        })
-        $('.errorText').each(function(){
-            if(inputBool[1] === false) return;
-            if($(this).hasClass('active')){
-                inputBool[1] = false;
-                return;
-            }
-            inputBool[1] = true;
-        })
-        let result = inputBool.every((test)=>{
-            return test === true
-        })
-        if(result){
-            $('input[type="submit"]').addClass('active');
-        }
+
+    $('[data-input="password-re"]').on('input' ,function(){
+        $(this).val() === $('[data-input="password"]').val() ? $(this).next().addClass('active') : $(this).next().removeClass('active')
+
+    })
+    $('input[type="submit"]').click(function(e){
+        !$(this).hasClass('active') && e.preventDefault();
+    })
+}
+
+// 유효성 검사 통과하면 submit 색상
+function submitActive(){
+    let inputBool = [];
+    inputBool.push($('input[type="text"] , input[type="password"] , input[type="number"]').get().every((a)=>{
+        return $(a).val() !== '';
+    }))
+    inputBool.push($('.errorText').get().every((a)=>{
+        return !$(a).hasClass('active');
+    }))
+    let result = inputBool.every((test)=>{
+        return test === true
+    })
+    if(result){
+        $('input[type="submit"]').addClass('active');
     }
-   
-})
+}
