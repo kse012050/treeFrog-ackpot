7309f2fffab343b744fae6d392f71a743cc75968
김성은 <kse012050@naver.com>
2022-12-26 10:42:08 +0900

    핸드폰번호 변동 페이지 input

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     // 인풋 유효성 검사
     inputValidation()
+
+    submitClick();
 })
 
 // 인풋 유효성 검사
@@ -18,44 +20,84 @@ function inputValidation(){
         $(this).val() === $('[data-input="password"]').val() ? $(this).next().addClass('active') : $(this).next().removeClass('active')
         submitActive();
     })
+}
+
+// 유효성 검사 통과하면 submit 색상
+function submitActive(){
+    let inputBool = [];
+
+    // input 값이 입력되었는지 확인
+    inputBool.push($('input[type="text"] , input[type="password"] , input[type="number"]').get().every((a)=>{
+        return $(a).val() !== '';
+    }))
+
+    // 에러메세지가 있는지 없는지 확인
+    inputBool.push(errorTextConfirm());
+    
+    // input 값 , 에러메세지가 둘 다 true인지 확인
+    let result = inputBool.every((test)=>{
+        return test === true
+    })
+
+    // input 값 , 에러메세지가 둘 다 true 면 submit 버튼에 active 클래스 추가
+    result && $('input[type="submit"]').addClass('active');
+}
+
+function errorTextConfirm(){
+    return $('.errorText').get().every((a)=>{
+        return !$(a).hasClass('active');
+    })
+}
+
+// submit 클릭 이벤트
+function submitClick(){
+    let inputAttr = 'name';
+    let testID = '01092931656';
+    let testPW = '123456'
+    let inputValue = [{}]
     $('input[type="submit"]').click(function(e){
         !$(this).hasClass('active') && e.preventDefault();
 
         // 폼으로 데이터 전송 시 삭제
         e.preventDefault();
 
-        let inputAttr = 'name';
-        var testID = '01092931656';
-        var testPW = '123456'
-
         // 인풋에서 받아 온 값
-        var inputValue = {}
-        $(this).parents('form').find('input:not([type="submit"])').each(function(){
+        console.log($(this).parents('form').find('input:not([type="submit"])'));
+        $(this).parents('form').find('input:not([type="submit"])').each(function(i){
             if(!$(this).attr(inputAttr)) return;
-            inputValue[$(this).attr(inputAttr)] = $(this).val();
+            inputValue[i] = {
+                selector : $(this),
+                name : $(this).attr(inputAttr),
+                value : $(this).val()
+            };
         })
-
-        // 로그인 페이지
-        $('#signIn').click(function(){
-            inputValue.userMobile !== testID && $(`input[${inputAttr}="userMobile"]`).focus().next('p.errorText').addClass('active');
-        })
+        
+        $(this).attr('id') === 'signIn' && mobileAndPW('signIn');
+        $(this).attr('id') === 'mobileChange' && mobileAndPW('mobileChange');
 
     })
-}
 
-// 유효성 검사 통과하면 submit 색상
-function submitActive(){
-    let inputBool = [];
-    inputBool.push($('input[type="text"] , input[type="password"] , input[type="number"]').get().every((a)=>{
-        return $(a).val() !== '';
-    }))
-    inputBool.push($('.errorText').get().every((a)=>{
-        return !$(a).hasClass('active');
-    }))
-    let result = inputBool.every((test)=>{
-        return test === true
-    })
-    if(result){
-        $('input[type="submit"]').addClass('active');
+    // 로그인 페이지 submit 클릭
+    function mobileAndPW(pageName){
+        // 값 판별
+        inputValue.map((v)=>{
+            v.name === 'userMobile' && (v.value !== testID && v.selector.next('p.errorText').addClass('active'));
+            v.name === 'userPassword' && (v.value !== testPW && v.selector.next('p.errorText').addClass('active'));
+        })
+
+        // 값이 맞지 않으면 값이 맞지 않는 첫번째 input 포커스
+        inputValue.find((v)=>{
+            return v.selector.next('p.errorText').hasClass('active') && v.selector.focus();
+        })
+
+        // 에러 메세지가 없으면
+        var result = errorTextConfirm();
+
+        // 페이지 이동
+        if(result){
+            pageName === 'signIn' && (location.href = '../index.html');
+            pageName === 'mobileChange' ? (location.href = 'newMobile.html') : $('.popupArea').addClass('active');
+        }
     }
 }
+
